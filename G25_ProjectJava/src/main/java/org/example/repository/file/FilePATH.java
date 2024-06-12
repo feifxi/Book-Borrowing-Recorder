7875ca6da31c15ce8489fdec3a43f134d95ea319
Chanombude <137422903+feifxi@users.noreply.github.com>
2024-06-12 14:34:16 +0700

    Update FilePATH.java

--- G25_ProjectJava/src/main/java/org/example/repository/file/FilePATH.java
+++ G25_ProjectJava/src/main/java/org/example/repository/file/FilePATH.java
@@ -3,5 +3,5 @@ package org.example.repository.file;
 
 public class FilePATH {
     //  Set path for file storage here!
-    public static String path = "C:\\Users\\feoy1\\Downloads";
+    public static String path = "C:\\Users\\User555\\Downloads";
 }
